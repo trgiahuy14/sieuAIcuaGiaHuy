c3c9d6af74727a1731a94e45abbca41fe78a6876
trgiahuy14 <trgiahuy14@gmail.com>
2022-11-18 11:01:56 +0700

    ver2hoanthien

--- tinhbmi/assets/js/main.js
+++ tinhbmi/assets/js/main.js
@@ -6,10 +6,10 @@ btn.addEventListener('click', () =>{
     const nhanxet =document.getElementById("nhanxet");
     bmi = (cannang / ((chieucao * chieucao) / 10000)).toFixed(2);
     tinh.innerHTML = "Bmi của bạn là " + bmi;
-    if (bmi<18.5){
+    if (bmi<21){
         tinh.innerHTML = bmi;
         nhanxet.innerHTML = "Bạn nghiện chắc luôn :))"
-    } else if(18.5 < bmi < 24.9){
+    } else if( bmi < 23.9){
         tinh.innerHTML = bmi;
         nhanxet.innerHTML = "Bạn bình thường"
     } else {
